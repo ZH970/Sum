4354113ead10dedd52d53a9f6e904dc720d0d6db
ZH970 <1210106679@qq.com>
2022-09-06 11:06:26 +0800

    first commit

---
+++ exercise/exercise.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+int main()
+{
+	int i;
+	int sum = 0;
+	char ch[2] = {'z'};//初始化
+	printf("Plese inter some numbers with space to seperate:");
+	while (true)
+	{
+		if (scanf_s("%d", &i) == 1)
+		{
+			sum += i;
+			while ((ch[0] = getchar()) == ' ')
+			{
+				;//Nothing to do to skip the space.
+			}
+			if (ch[0] == '\n')
+			{
+	break;
+			}
+			ungetc(ch[0], stdin);
+		}
+		else
+		{
+			MessageBox(
+				NULL,
+				(LPCWSTR)L"Plese inter number or some space instead of others.\nDo you want to try again?",
+				(LPCWSTR)L"Fail",
+				MB_OK|MB_SYSTEMMODAL
+			);
+			exit(0);
+		}
+	}
+	printf("The result is %d", sum);
+}
